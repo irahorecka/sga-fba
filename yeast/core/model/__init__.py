f3015aebfc71f272d0857bac900ba4086a2d1630
Ira Horecka <ira89@icloud.com>
2022-03-31 16:01:49 -0700

    update yeast/core/model --> automatically load yeast-GEM-8.5.0.xml model without filepath param

--- yeast/core/model/__init__.py
+++ yeast/core/model/__init__.py
@@ -1,10 +1,18 @@
+from pathlib import Path
+
 import cobra
 
 from yeast.util.model import optimize
 
+MODEL_DIR = Path(__file__).resolve().parent
+
+
+def get_objective_value(model):
+    return model.optimize().objective_value
+
 
 @optimize
-def load_model(model_path, solver="cplex"):
-    model = cobra.io.read_sbml_model(model_path)
+def load_model(solver="cplex"):
+    model = cobra.io.read_sbml_model(str(MODEL_DIR / "yeast-GEM-8.5.0.xml"))
     model.solver = solver
     return model
