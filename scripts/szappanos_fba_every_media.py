a60d5a63951e619f66c97013a6f5bf25a14430b7
Ira Horecka <ira89@icloud.com>
2022-04-05 10:44:54 -0700

    update scripts/szappanos_fba_every_media.py to accept config constants from config.yaml

--- scripts/szappanos_fba_every_media.py
+++ scripts/szappanos_fba_every_media.py
@@ -4,81 +4,78 @@ from datetime import datetime
 from pathlib import Path
 from pprint import pprint
 
-ROOT_DIR = Path().resolve().parent
+ROOT_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(ROOT_DIR))
 
+import yaml
 from cobra.flux_analysis import double_gene_deletion, single_gene_deletion
 
-from scripts.utils import export_deletion_flux_as_tsv, write_json
-from yeast.core.gene import assess_gene_knockout_viability, knockout_genes
+from scripts.utils import export_df_as_tsv, write_json
+from yeast.core.gene import knockout_genes
 from yeast.core.gene.essential import attenuate_flux
 from yeast.core.media import apply_media, get_every_media
 from yeast.core.model import get_objective_value, load_model
 
+with open(Path(__file__).resolve().parent / "config.yaml", "r", encoding="utf-8") as config:
+    YEAST_FBA = yaml.safe_load(config)["yeast-fba"]
+
 
 def main(model_description=""):
     # Apply media as constructed in in silico model from Szappanos et al., 2011 paper
-    for medium_name, medium in get_every_media().items():
+    media = get_every_media()
+    if YEAST_FBA["media"]:
+        media = {medium_name: media[medium_name] for medium_name in YEAST_FBA["media"]}
+    for medium_name, medium in media.items():
         # Load model and apply medium
-        print("loading model...")
         model = load_model()
-        print(f"applying media {medium_name}...")
         model = apply_media(model, medium)
 
-        # URA3 ('YEL021W') and LEU2 ('YCL018W')
-        # KO of YPR021 derived from Heavner and Price - 2015 - update: doesn't help the model
-        ko_genes = ["YEL063C", "YNL268W", "YLR303W", "YEL021W", "YCL018W"]
-        non_lethal_ko_genes = [
-            gene for gene in ko_genes if assess_gene_knockout_viability(model, gene) is True
-        ]
-        lethal_genes = list(set(ko_genes).symmetric_difference(set(non_lethal_ko_genes)))
+        # Perform gene knockout(s)
+        knockout = {
+            gene: flux for gene, flux in YEAST_FBA["gene"]["deletions"].items() if flux == 0
+        }
+        summary = {
+            "medium": medium_name,
+            "gene-knockout": knockout,
+            "gene-knockout-order": list(knockout.keys()),
+        }
+        model = knockout_genes(model, list(knockout.keys()))
 
-        # Gene knockout
-        print("non-essential genes to delete:", non_lethal_ko_genes)
-        summary = {"medium": medium_name, "non-lethal-gene-ko": non_lethal_ko_genes}
-        model = knockout_genes(model, non_lethal_ko_genes)
-        print("essential genes to attenuate to 80% fitness each:", lethal_genes)
-        summary["lethal-gene-attenuate"] = lethal_genes
-
-        # The order in which genes are attenuated MATTERS
-        model = attenuate_flux(model, "YCL018W", flux_ratio=0.8)
-        model = attenuate_flux(model, "YEL021W", flux_ratio=0.85)
-
-        # for gene in lethal_genes:
-        #     model = attenuate_flux(model, gene, flux_ratio=0.8)
+        # Perform gene attenuation(s)
+        attenuate = {
+            gene: flux for gene, flux in YEAST_FBA["gene"]["deletions"].items() if flux > 0
+        }
+        for gene, flux in attenuate.items():
+            model = attenuate_flux(model, gene, flux_ratio=flux)
+        summary = {
+            **summary,
+            **{"gene-attenuate": attenuate, "gene-attenuate-order": list(attenuate.keys())},
+        }
 
         # Find WT growth
         wt_fitness = get_objective_value(model)
-        print("WT fitness is:", wt_fitness)
         summary["wt-fitness"] = wt_fitness
 
-        # =======================================================
-        # Perform every single gene knockout
-        print("performing total single gene knockout...")
+        # Perform total single and double gene knockout
         single_gene_deletions_szappanos = single_gene_deletion(model)
-        # Perform every double gene knockout
-        # print("performing total double gene knockout...")
         # double_gene_deletions_szappanos = double_gene_deletion(model)
 
         # Generate appropriate filename
-        date = datetime.now().strftime("%Y%m%d")
-        dirname = f"{model_description}_{medium_name}_{date}"
+        dirname = f"{model_description}_{medium_name}_{datetime.now().strftime('%Y%m%d')}"
         if not os.path.exists(dirname):
             os.mkdir(dirname)
         json_filename = f"{dirname}.json"
         tsv_filename = f"{dirname}.tsv"
-
         single_gene_ko_tsv_filename = f"single_gene_ko_{tsv_filename}"
         # double_gene_ko_tsv_filename = f"double_gene_ko_{tsv_filename}"
         summary["single-gene-ko-tsv-filename"] = single_gene_ko_tsv_filename
         # summary["double-gene-ko-tsv-filename"] = double_gene_ko_tsv_filename
 
-        # Export
-        print("exporting as tsv...")
-        export_deletion_flux_as_tsv(
+        # Export TSV and JSON files
+        export_df_as_tsv(
             single_gene_deletions_szappanos, os.path.join(dirname, single_gene_ko_tsv_filename)
         )
-        # export_deletion_flux_as_tsv(
+        # export_df_as_tsv(
         #     double_gene_deletions_szappanos, os.path.join(dirname, double_gene_ko_tsv_filename)
         # )
         write_json(summary, os.path.join(dirname, json_filename))
@@ -86,4 +83,4 @@ def main(model_description=""):
 
 
 if __name__ == "__main__":
-    main(model_description="szappanos")
+    main(model_description=YEAST_FBA["model-description"])
